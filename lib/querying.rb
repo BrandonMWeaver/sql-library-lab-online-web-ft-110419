784d02e6b3b1f1fefb1863a9b01b59c84616c987
Brandon Weaver <a7xncob@gmail.com>
2019-12-10 19:07:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,8 +27,8 @@ def select_series_title_with_most_human_characters
    JOIN authors ON series.author_id = authors.id
    JOIN characters ON authors.id = characters.author_id
    WHERE characters.species = \"human\"
-   GROUP BY series
-   ORDER BY series.title;"
+   GROUP BY series.id
+   ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
