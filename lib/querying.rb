93f2dd915e21db29c327b93d2b877456e3d3e73c
Brandon Weaver <a7xncob@gmail.com>
2019-12-10 18:51:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,5 +30,6 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(books) FROM characters
-   JOIN character_books ON characters.id = character_books.character_id"
+   JOIN character_books ON characters.id = character_books.character_id
+   JOIN books ON character_books.book_id = books.id;"
 end
