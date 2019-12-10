387ead54d4fa0e676ba1f4b51641a5a4447cfed5
Brandon Weaver <a7xncob@gmail.com>
2019-12-10 18:53:13 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,5 +33,5 @@ def select_character_names_and_number_of_books_they_are_in
    JOIN character_books ON characters.id = character_books.character_id
    JOIN books ON character_books.book_id = books.id
    GROUP BY characters.id
-   ORDER BY COUNT(books.title);"
+   ORDER BY COUNT(books.title) DESC;"
 end
