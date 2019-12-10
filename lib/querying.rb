1dd762d020106ba6b45efbe9266c151f7f6447f9
Brandon Weaver <a7xncob@gmail.com>
2019-12-10 18:58:10 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
-   JOIN books ON series.
+   JOIN books ON series.id = books.series_id
    JOIN characters ON series.id = characters"
 end
 
